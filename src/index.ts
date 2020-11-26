41de32f8bb79c82b3fa6cf90e098a14be4322469
Tony <tony.js@zoho.eu>
2020-11-26 18:25:32 -0300

    feat: initial lib

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log( 'Hello world' )
+import React from 'react'
+
+const useRefs = <T>( ...refs: readonly React.Ref<T>[] ): React.Ref<T> => {
+  return React.useCallback( ( reference: T ) => {
+    refs.forEach( ref => {
+      if ( typeof ref === 'function' ) ref( reference )
+      else if ( ref ) Object.assign( ref, { current: reference } )
+    } )
+  }, refs )
+}
+
+export default useRefs
